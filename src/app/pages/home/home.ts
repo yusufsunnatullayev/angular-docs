0faef674ad74f4fbc62b63fecef90d7891d4f9fe
Yusuf <s.yusuf@uniconsoft.uz>
2026-06-19 10:11:34 +0500

    first commit

---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Home {
+
+}
---
+++ src/app/pages/signals/signals.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signals',
+  imports: [],
+  templateUrl: './signals.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Signals {
+
+}
